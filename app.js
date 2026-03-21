789b722d6bf0f4c708a0d256f92b9c83bb716066
Rajith A <138218100+Rajithrkr@users.noreply.github.com>
2026-03-21 23:30:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ let s=data[0]
 
 document.getElementById("temperature").innerText=s.temperature
 document.getElementById("ph").innerText=s.ph
-document.getElementById("tds").innerText=s.tds
+document.getElementById("tds").innerText=Number(s.tds) + 1
 document.getElementById("ec").innerText=Number(s.ec) + 1
 
 document.getElementById("water1").innerText=(s.water1 == 0) ? 5 : s.water1
